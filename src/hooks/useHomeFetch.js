388504e3577e505a4a78ca12db06479ed74d1e6c
Genevieve <vievenavales29@gmail.com>
2021-09-21 21:32:04 +0800

    grabbing from session storage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -3,6 +3,9 @@ import { useState, useEffect, useRef } from "react";
 // API
 import API from "../API";
 
+// Helpers
+import { isPersistedState } from "../helpers";
+
 const initialState = {
   page: 0,
   results: [],
@@ -39,6 +42,17 @@ export const useHomeFetch = () => {
 
   // Search
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState("homeState");
+
+      if (sessionState) {
+        console.log("grabbing from session storage");
+        setState(sessionState);
+        return;
+      }
+    }
+
+    console.log("grabbing from API");
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -50,6 +64,11 @@ export const useHomeFetch = () => {
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to session storage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem("homeState", JSON.stringify(state));
+  }, [searchTerm, state]);
 
   return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
